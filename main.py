3f6d3472fda11fc939a7057f0b631662868f9f3b
Milton Bravo <miltonbravo96@gmail.com>
2024-11-18 19:12:01 -0600

    busqueda de usuarios por parametros

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from database import test_connection, select
 
 
 def main() -> None:
-    test_connection()
-    usuarios = select("select * from usuarios;")
-    print(usuarios)
+    try:
+        test_connection()
+        usuarios = select("select * from usuarios where usuario = ?;", ('a',))
+        print(usuarios)
+        lista_usuarios = select("select * from usuarios;")
+        print(lista_usuarios)
+    except Exception as ex:
+        print(ex)
 
 if __name__ == "__main__":
     main()
