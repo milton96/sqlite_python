70e721d2058d591f652fc78b6952991fb1740e51
Milton Bravo <miltonbravo96@gmail.com>
2024-11-19 00:32:19 -0600

    avance registro usuario

--- resources/usuarioResource.py
+++ resources/usuarioResource.py
@@ -1,6 +1,9 @@
+from flask import request
 from flask_restful import Resource, abort
 
+
 import services.usuarioService as userSrv
+from validations.usuarioValidation import nuevo_usuario
 
 class UsuariosResource(Resource):
     def get(self):
@@ -8,6 +11,16 @@ class UsuariosResource(Resource):
             usuarios = userSrv.lista()
             return usuarios, 200
         except Exception as ex:
+            abort(400, description="Ocurrio un error inesperado")
+
+    def post(self):
+        try:
+            body = request.json
+            u = nuevo_usuario(body)
+            print(u)
+            return "", 204
+        except Exception as ex:
+            print(ex)
             abort(400, description="Ocurrio un error inesperado")
         
 class UsuarioResource(Resource):
