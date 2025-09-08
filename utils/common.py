0440a10b9d82bbb619a65a3de8633a24d446462b
Milton Bravo <miltonbravo96@gmail.com>
2025-09-07 18:48:02 -0600

    se agrega funcion de validacion de error

--- utils/common.py
+++ utils/common.py
@@ -1,5 +1,18 @@
 from hashlib import sha256
 
+from pydantic import ValidationError
+
+from utils.customException import ExceptionControlada
+
 def to_sha256(string: str) -> str:
     hash_string = sha256(string.encode()).hexdigest()
     return hash_string
+
+def validate_error(ve: ValidationError):
+    detail = ve.errors()[0]
+    ctx = detail.get("ctx")
+    if (ctx is not None):
+        error = ctx.get("error")
+        raise ExceptionControlada(400, error.__str__())
+    
+    raise ExceptionControlada(500, "Ocurrio un error desconocido en las validaciones")
