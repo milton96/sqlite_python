184334665c98d03e9f9a124ff69ef92072fd65bb
Milton Bravo <miltonbravo96@gmail.com>
2025-09-07 18:48:47 -0600

    se implementa exceopcion personalizada y se mejora la respeusta

--- resources/usuarioResource.py
+++ resources/usuarioResource.py
@@ -1,8 +1,9 @@
-from flask import request
+from flask import Response, request
 from flask_restful import Resource, abort
 
 
 import services.usuarioService as userSrv
+from utils.customException import ExceptionControlada
 from validations.usuarioValidation import nuevo_usuario
 
 class UsuariosResource(Resource):
@@ -19,16 +20,17 @@ class UsuariosResource(Resource):
             u = nuevo_usuario(body)
             print(u)
             return "", 204
+        except ExceptionControlada as ec:
+            abort(ec.codigo, description=ec.message)
         except Exception as ex:
-            print(ex)
-            abort(400, description="Ocurrio un error inesperado")
+            abort(500, description="Ocurrio un error inesperado")
         
 class UsuarioResource(Resource):
     def get(self, id):
         try:
             usuario = userSrv.obtener(id)
             if usuario is None:
-                return "", 204
+                return Response(status=404)
             return usuario, 200
         except Exception as ex:
             abort(400, description="Ocurrio un error inesperado")
@@ -36,6 +38,6 @@ class UsuarioResource(Resource):
     def delete(self, id):
         try:
             userSrv.cambiar_activo(id, 0)
-            return "", 204
+            return Response(status=204)
         except Exception as ex:
             abort(400, description="Ocurrio un error inesperado")
